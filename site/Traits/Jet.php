f0da82cbb536989a8a7520808e75685617a9ff89
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-27 16:41:02 +0300

    New: added setter for oPstatus in Jet Trait

--- site/Traits/Jet.php
+++ site/Traits/Jet.php
@@ -1,1 +1,30 @@
 <?php
+
+require_once 'airliner.php';
+require_once 'BusinessJet.php';
+
+trait Jet{
+    use Airliner,BusinessJet{
+        BusinessJet::setOperator insteadof Airliner;
+        Airliner::setOperator as setOp;
+    }
+
+    public static $oPStatus = "In service";
+    public $operationBase;
+
+    /**
+     * @param string $oPStatus
+     */
+    public static function setOPStatus($oPStatus)
+    {
+        self::$oPStatus = $oPStatus;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getOPStatus()
+    {
+        return self::$oPStatus;
+    }
+}
